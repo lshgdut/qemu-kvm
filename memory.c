055c94ea206d6ff7df59b401bdc9d51e31cd0c5d
Avi Kivity <avi@redhat.com>
2011-09-14 12:10:12 +0300

    memory: introduce memory_region_set_address()
    Allow changing the address of a memory region while it is
    in the memory hierarchy.
    
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- memory.c
+++ memory.c
@@ -1318,6 +1318,26 @@ void memory_region_set_enabled(MemoryRegion *mr, bool enabled)
     memory_region_update_topology(NULL);
 }
 
+void memory_region_set_address(MemoryRegion *mr, target_phys_addr_t addr)
+{
+    MemoryRegion *parent = mr->parent;
+    unsigned priority = mr->priority;
+    bool may_overlap = mr->may_overlap;
+
+    if (addr == mr->addr || !parent) {
+        return;
+    }
+
+    memory_region_transaction_begin();
+    memory_region_del_subregion(parent, mr);
+    if (may_overlap) {
+        memory_region_add_subregion_overlap(parent, addr, mr, priority);
+    } else {
+        memory_region_add_subregion(parent, addr, mr);
+    }
+    memory_region_transaction_commit();
+}
+
 void set_system_memory_map(MemoryRegion *mr)
 {
     address_space_memory.root = mr;
